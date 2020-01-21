5590bec77f3ec26ab65e22e7c27aa42f1df4d07f
Roel Bouman <roelbouman@gmail.com>
2020-01-21 15:02:11 +0100

    first commit with first useful script

---
+++ clear_shared_memory_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f /mnt/spark-data/beamformed/*
+rm -f /mnt/spark-data/weather/*
+rm -f /mnt/spark-data/metadata/*
+
+rm -f /mnt/spark-results/medians/*
+rm -f /mnt/spark-results/median_scaled_data/*
+rm -f /mnt/spark-results/power_filter/*
